fa458614a469c13c83165a83d25de9cef7d4fae8
GUSTAVO PIZATTO <gustavopizatto@hotmail.com>
2025-08-04 12:31:10 -0300

    Update config.js
    Arquivo comentado

--- mapcon/config.js
+++ mapcon/config.js
@@ -1,3 +1,8 @@
+// Importa a biblioteca 'dotenv', que serve para carregar variáveis de ambiente
 const dotenv = require('dotenv');
- 
+
+// Configura o dotenv para carregar um arquivo .env específico com base no ambiente atual da aplicação
+// Por exemplo:
+// Se NODE_ENV = 'development', ele carregará as variáveis de .env.development
+// Se NODE_ENV = 'production', ele carregará as variáveis de .env.production
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
